82a34a4c3443e5349863b8049679aa216e2b1722
BrookeBryski <brookeabryski@gmail.com>
2022-01-20 15:46:46 -0600

    inital commit

---
+++ src/main/java/com/example/SpringBootRecordStore/SpringBootRecordStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootRecordStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecordStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRecordStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootRecordStore/SpringBootRecordStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootRecordStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRecordStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
