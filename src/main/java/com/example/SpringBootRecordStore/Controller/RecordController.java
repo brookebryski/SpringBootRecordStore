c687fe7779e29b8f02e75a62ef4a679f65711d95
BrookeBryski <brookeabryski@gmail.com>
2022-01-24 12:21:05 -0600

    addResponse

--- src/main/java/com/example/SpringBootRecordStore/Controller/RecordController.java
+++ src/main/java/com/example/SpringBootRecordStore/Controller/RecordController.java
@@ -3,6 +3,9 @@ package com.example.SpringBootRecordStore.Controller;
 import com.example.SpringBootRecordStore.Repository.RecordRepository;
 import com.example.SpringBootRecordStore.Service.RecordService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,4 +16,7 @@ public class RecordController {
 
     @Autowired
     RecordService recordService;
+
+    @PostMapping("/addRecord")
+    public ResponseEntity<AddResponse> addRecordImplementation(@RequestBody Record record)
 }
