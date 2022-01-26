3fca012044bb6fcc9b26ee75a488b15b94ce8856
BrookeBryski <brookeabryski@gmail.com>
2022-01-26 13:15:39 -0600

    implemented mockmvc and wrote addRecordControllerTest

--- src/test/java/com/example/SpringBootRecordStore/SpringBootRecordStoreApplicationTests.java
+++ src/test/java/com/example/SpringBootRecordStore/SpringBootRecordStoreApplicationTests.java
@@ -5,17 +5,28 @@ import com.example.SpringBootRecordStore.Controller.Record;
 import com.example.SpringBootRecordStore.Controller.RecordController;
 import com.example.SpringBootRecordStore.Repository.RecordRepository;
 import com.example.SpringBootRecordStore.Service.RecordService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.RequestEntity.post;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+
 class SpringBootRecordStoreApplicationTests {
 
 	@Autowired
@@ -24,6 +35,8 @@ class SpringBootRecordStoreApplicationTests {
 	RecordRepository repository;
 	@MockBean
 	RecordService recordService;
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
@@ -44,6 +57,7 @@ class SpringBootRecordStoreApplicationTests {
 		Record rec = buildRecord();
 		when(recordService.buildId(rec.getIsmn(), rec.getAisle())).thenReturn(rec.getId());
 		when(recordService.checkRecordAlreadyExists(rec.getId())).thenReturn(false);
+		when(repository.save(any())).thenReturn(rec);
 
 		ResponseEntity response =con.addRecordImplementation(buildRecord());
 		System.out.println(response.getStatusCode());
@@ -52,6 +66,20 @@ class SpringBootRecordStoreApplicationTests {
 		ad.getId();
 		assertEquals(rec.getId(), ad.getId());
 		assertEquals("Success Record is Added", ad.getMsg());
+	}
+
+	@Test
+	public void addRecordControllerTest()
+	{
+		Record rec = buildRecord();
+		ObjectMapper map =new ObjectMapper();
+		String jsonString = map.writeValueAsString(rec);
+
+		when(recordService.buildId(rec.getIsmn(), rec.getAisle())).thenReturn(rec.getId());
+		when(recordService.checkRecordAlreadyExists(rec.getId())).thenReturn(false);
+		when(repository.save(any())).thenReturn(rec);
+		this.mockMvc.perform(post("/addRecord")).contentType(MediaType.APPLICATION_JSON);
+		.content(jsonString).andExpect(status().isCreated());
 	}
 
 	public Record buildRecord()
